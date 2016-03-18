c403903bafb0e83bdf8ca1a186a3b4062824656b
Chris Jones <chrisjo@zillow.com>
2016-03-18 12:34:40 -0700

    bug

--- app/controllers/api/passwords_controller.rb
+++ app/controllers/api/passwords_controller.rb
@@ -23,7 +23,7 @@ class Api::PasswordsController < Api::BaseController
   def slack
     @password = Password.new(password: slack_params[:text])
     if @password.save
-      HTTParty.get('https://slack.com/api/chat.postMessage?token=' + Rails.application.secrets.slack_api_token + '&channel=' + slack_params[:channel] + '&text=' + polymorphic_url(@password) + '&unfurl_links=false'
+      HTTParty.get('https://slack.com/api/chat.postMessage?token=' + Rails.application.secrets.slack_api_token + '&channel=' + slack_params[:channel] + '&text=' + polymorphic_url(@password) + '&unfurl_links=false')
     end
   end
 
